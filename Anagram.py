dc9532066e824649502390ca6433ba5274d667d1
Aryan-1712 <136790925+Aryan-1712@users.noreply.github.com>
2024-12-08 11:06:45 +0530

    Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -1,1 +1,12 @@
-
+s1 = input("Enter String 1 = ")
+s2 = input("Enter String 2 = ")
+L_1 = []
+L_2 = []
+for i in range(0, len(s1)):
+    L_1.append(s1[i].lower())
+for i in range(0, len(s2)):
+    L_2.append(s2[i].lower())
+if L_1 == L_2:
+    print("They are Anagram")
+else:
+    print("They are not Anagram")
