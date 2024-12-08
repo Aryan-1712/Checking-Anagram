0e0d9e26b5e5950faa46329f48af3cabcefbcbbb
Aryan-1712 <136790925+Aryan-1712@users.noreply.github.com>
2024-12-08 11:13:00 +0530

    Anagram.py

--- Anagram.py
+++ Anagram.py
@@ -6,6 +6,8 @@ for i in range(0, len(s1)):
     L_1.append(s1[i].lower())
 for i in range(0, len(s2)):
     L_2.append(s2[i].lower())
+L_1.sort()
+L_2.sort()
 if L_1 == L_2:
     print("They are Anagram")
 else:
